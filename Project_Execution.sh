5e2f8a1f967dd305fef4c4dbc2a5c487addf9e47
liveabilityment360 <109857290+liveabilityment360@users.noreply.github.com>
2022-09-10 01:39:46 +1000

    Update Project_Execution.sh

--- Project_Execution.sh
+++ Project_Execution.sh
@@ -1,41 +1,41 @@
 #1.create a project and export it into a variable(project creation- manual)
-export PROJECT="liveability-demo-034"
+export PROJECT_ID="liveability-demo-033"
       
 #2.Sets the project,location &dataset
-gcloud config set project $PROJECT
+gcloud config set project ${PROJECT_ID}
 export LOCATION="us-west1"
 export BQ_DATASET="liveability"
 
 #3.Create the service account with same proj name and export it to a variable which can be used in later stages    
-gcloud iam service-accounts create $PROJECT \
+gcloud iam service-accounts create ${PROJECT_ID} \
  --description="Service account for the Liveability project" \
  --display-name="Liveability Service Account"    
 
-export SERVICE_ACCOUNT_ID=$PROJECT
+export SERVICE_ACCOUNT_ID=${PROJECT_ID}
 
 # 4.Assign different roles to the services account.
 
 #dataflow
-gcloud projects add-iam-policy-binding $PROJECT --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT.iam.gserviceaccount.com --role=roles/dataflow.admin
-gcloud projects add-iam-policy-binding $PROJECT --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT.iam.gserviceaccount.com --role=roles/bigquery.admin
-gcloud projects add-iam-policy-binding $PROJECT --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT.iam.gserviceaccount.com --role=roles/datastore.user
-gcloud projects add-iam-policy-binding $PROJECT --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT.iam.gserviceaccount.com --role=roles/storage.admin
-gcloud projects add-iam-policy-binding $PROJECT --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT.iam.gserviceaccount.com --role=roles/iam.serviceAccountUser
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/dataflow.admin
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/bigquery.admin
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/datastore.user
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/storage.admin
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/iam.serviceAccountUser
 
 #datastream
-gcloud projects add-iam-policy-binding $PROJECT --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT.iam.gserviceaccount.com --role=roles/storage.objectViewer
-gcloud projects add-iam-policy-binding $PROJECT --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT.iam.gserviceaccount.com --role=roles/storage.objectCreator
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/storage.objectViewer
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/storage.objectCreator
     
 #cloud sql
-gcloud projects add-iam-policy-binding $PROJECT --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT.iam.gserviceaccount.com --role=roles/cloudsql.admin
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/cloudsql.admin
 
 #pub/sub
-gcloud projects add-iam-policy-binding $PROJECT --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT.iam.gserviceaccount.com --role=roles/pubsub.subscriber
-gcloud projects add-iam-policy-binding $PROJECT --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT.iam.gserviceaccount.com --role=roles/pubsub.admin
-gcloud projects add-iam-policy-binding $PROJECT --member=serviceAccount:$SERVICE_ACCOUNT_ID@$PROJECT.iam.gserviceaccount.com --role=roles/pubsub.viewer
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/pubsub.subscriber
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/pubsub.admin
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/pubsub.viewer
 
 #5.Creating a service account key
-gcloud iam service-accounts keys create key.json --iam-account=$SERVICE_ACCOUNT_ID@$PROJECT.iam.gserviceaccount.com
+gcloud iam service-accounts keys create key.json --iam-account=${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com
 export GOOGLE_APPLICATION_CREDENTIALS=key.json
 
 #6.Enable all the APIs
@@ -54,22 +54,22 @@ gcloud services enable datastream.googleapis.com
 gcloud services enable pubsub.googleapis.com
 
 #7.Create fire store DB for schema to be used in dataflow
-gcloud app create --region=$LOCATION
-gcloud firestore databases create --region=$LOCATION
+gcloud app create --region=${LOCATION}
+gcloud firestore databases create --region=${LOCATION}
 
 #8.Generates a GCS bucket with the project id.
-gsutil mb -l $LOCATION gs://$PROJECT
+gsutil mb -l ${LOCATION} gs://${PROJECT_ID}
     
 #9.Create a datset in bq
-bq --location=$LOCATION mk \
- --dataset $BQ_DATASET 
+bq --location=${LOCATION} mk \
+ --dataset ${BQ_DATASET} 
 
 #10.Clone the data and schema from git to cloud shell
 git clone https://github.com/liveabilityment360/final_demo
 
 #11. Copy the files to cloud storage
-gsutil cp final_demo/data/* gs://$PROJECT/data/batch_data/
-gsutil cp final_demo/ddl/* gs://$PROJECT/ddl/
+gsutil cp final_demo/data/* gs://${PROJECT_ID}/data/batch_data/
+gsutil cp final_demo/ddl/* gs://${PROJECT_ID}/ddl/
 
 #12.Create the SQL instance using terraform
 cd final_demo/terraform
@@ -102,18 +102,18 @@ export DS_TARGET_JSON="final_demo/gcs_destination_user_activities_config.json"
 
 #16.Datastream user creation replica process and allowing privilleges for the 'datastream' user.
 SERVICE_ACCOUNT=$(gcloud sql instances describe ${MYSQL_INSTANCE} | grep serviceAccountEmailAddress | awk '{print $2;}')
-gsutil iam ch serviceAccount:$SERVICE_ACCOUNT:objectViewer gs://$PROJECT
-gcloud sql import sql ${MYSQL_INSTANCE} gs://$PROJECT/ddl/create-ds-user-privileges.sql --quiet
+gsutil iam ch serviceAccount:$SERVICE_ACCOUNT:objectViewer gs://${PROJECT_ID}
+gcloud sql import sql ${MYSQL_INSTANCE} gs://${PROJECT_ID}/ddl/create-ds-user-privileges.sql --quiet
 
 #17.Create table in mysql
-gcloud sql import sql ${MYSQL_INSTANCE} gs://$PROJECT/ddl/ddl_user_activity.sql --quiet
+gcloud sql import sql ${MYSQL_INSTANCE} gs://${PROJECT_ID}/ddl/ddl_user_activity.sql --quiet
 
 #18.Creates a topic for the user activity table from CloudSQL and create a subscription from the same topic.
 gcloud pubsub topics create ${DS_PUBSUB_TOPIC}
 gcloud pubsub subscriptions create ${DS_PUBSUB_SUBSCRIPTION} --topic=${DS_PUBSUB_TOPIC}
 
 #19.Create a notification to track the changes on the pub/sub topic -> under - Bucket_name -> PROJECT_ID and under folder -> data/stream_data
-gsutil notification create -f "json" -p "${DS_DIR_PATH}" -t "${DS_PUBSUB_TOPIC}" "gs://$PROJECT"
+gsutil notification create -f "json" -p "${DS_DIR_PATH}" -t "${DS_PUBSUB_TOPIC}" "gs://${PROJECT_ID}"
 
 #20.To get the IP address from the CloudSQL description
 MYSQL_IP_ADDRESS=$(gcloud sql instances describe ${MYSQL_INSTANCE} --format="value(ipAddresses.ipAddress)")
@@ -122,13 +122,16 @@ MYSQL_IP_ADDRESS=$(gcloud sql instances describe ${MYSQL_INSTANCE} --format="val
 gcloud datastream connection-profiles create ${MYSQL_CONN_PROFILE} --location=${LOCATION} --type=mysql --mysql-password=${MYSQL_PASS} --mysql-username=${MYSQL_USER} --display-name=${MYSQL_CONN_PROFILE} --mysql-hostname=${MYSQL_IP_ADDRESS} --mysql-port=${MYSQL_PORT} --static-ip-connectivity
 
 #22.Create a connection profile for the Google Cloud Storage Bucket.
-gcloud datastream connection-profiles create ${GCS_CONN_PROFILE} --location=${LOCATION} --type=google-cloud-storage --bucket=$PROJECT --root-path=${GCS_DS_PATH_PREFIX} --display-name=${GCS_CONN_PROFILE}
+gcloud datastream connection-profiles create ${GCS_CONN_PROFILE} --location=${LOCATION} --type=google-cloud-storage --bucket=${PROJECT_ID} --root-path=${GCS_DS_PATH_PREFIX} --display-name=${GCS_CONN_PROFILE}
 
 #23.Adding stream using the connection profiles. - Validation process --validate-only=true  
 gcloud datastream streams create ${DS_MYSQL_GCS_NAME} --location=${LOCATION} --display-name=${DS_MYSQL_GCS_NAME} --source=${MYSQL_CONN_PROFILE} --mysql-source-config=${DS_SOURCE_JSON} --destination=${GCS_CONN_PROFILE} --gcs-destination-config=${DS_TARGET_JSON} --backfill-none --validate-only
 
 #24.Generates the data stream.
 gcloud datastream streams create ${DS_MYSQL_GCS_NAME} --location=${LOCATION} --display-name=${DS_MYSQL_GCS_NAME} --source=${MYSQL_CONN_PROFILE} --mysql-source-config=${DS_SOURCE_JSON} --destination=${GCS_CONN_PROFILE} --gcs-destination-config=${DS_TARGET_JSON} --backfill-none
+
+#To enable datastream run
+gcloud datastream streams update ${DS_MYSQL_GCS_NAME} --location=${LOCATION} --state=RUNNING --update-mask=state
 
 #25.Run the streaming, need to start streaming manually
 gcloud beta dataflow flex-template run datastream-replication1 \
@@ -191,10 +194,10 @@ python3 data_ingestion_configurable.py \
 --max_num_workers=100 \
 --autoscaling_algorithm=THROUGHPUT_BASED \
 --region=us-west1 \
---staging_location=gs://$PROJECT/data/staging \
---temp_location=gs://$PROJECT/data/temp \
---project=$PROJECT \
---input-bucket=gs://$PROJECT/ \
+--staging_location=gs://${PROJECT_ID}/data/staging \
+--temp_location=gs://${PROJECT_ID}/data/temp \
+--project=${PROJECT_ID} \
+--input-bucket=gs://${PROJECT_ID}/ \
 --input-path=data/batch_data \
 --input-files=hospitals.csv \
 --bq-dataset=liveability
@@ -205,10 +208,10 @@ python3 data_ingestion_configurable.py \
 --max_num_workers=100 \
 --autoscaling_algorithm=THROUGHPUT_BASED \
 --region=us-west1 \
---staging_location=gs://$PROJECT/data/staging \
---temp_location=gs://$PROJECT/data/temp \
---project=$PROJECT \
---input-bucket=gs://$PROJECT/ \
+--staging_location=gs://${PROJECT_ID}/data/staging \
+--temp_location=gs://${PROJECT_ID}/data/temp \
+--project=${PROJECT_ID} \
+--input-bucket=gs://${PROJECT_ID}/ \
 --input-path=data/batch_data \
 --input-files=childcarecenters.csv \
 --bq-dataset=liveability
@@ -219,10 +222,10 @@ python3 data_ingestion_configurable.py \
 --max_num_workers=100 \
 --autoscaling_algorithm=THROUGHPUT_BASED \
 --region=us-west1 \
---staging_location=gs://$PROJECT/data/staging \
---temp_location=gs://$PROJECT/data/temp \
---project=$PROJECT \
---input-bucket=gs://$PROJECT/ \
+--staging_location=gs://${PROJECT_ID}/data/staging \
+--temp_location=gs://${PROJECT_ID}/data/temp \
+--project=${PROJECT_ID} \
+--input-bucket=gs://${PROJECT_ID}/ \
 --input-path=data/batch_data \
 --input-files=religiousorganizations.csv \
 --bq-dataset=liveability
@@ -233,10 +236,10 @@ python3 data_ingestion_configurable.py \
 --max_num_workers=100 \
 --autoscaling_algorithm=THROUGHPUT_BASED \
 --region=us-west1 \
---staging_location=gs://$PROJECT/data/staging \
---temp_location=gs://$PROJECT/data/temp \
---project=$PROJECT \
---input-bucket=gs://$PROJECT/ \
+--staging_location=gs://${PROJECT_ID}/data/staging \
+--temp_location=gs://${PROJECT_ID}/data/temp \
+--project=${PROJECT_ID} \
+--input-bucket=gs://${PROJECT_ID}/ \
 --input-path=data/batch_data \
 --input-files=restaurants.csv \
 --bq-dataset=liveability
@@ -247,10 +250,10 @@ python3 data_ingestion_configurable.py \
 --max_num_workers=100 \
 --autoscaling_algorithm=THROUGHPUT_BASED \
 --region=us-west1 \
---staging_location=gs://$PROJECT/data/staging \
---temp_location=gs://$PROJECT/data/temp \
---project=$PROJECT \
---input-bucket=gs://$PROJECT/ \
+--staging_location=gs://${PROJECT_ID}/data/staging \
+--temp_location=gs://${PROJECT_ID}/data/temp \
+--project=${PROJECT_ID} \
+--input-bucket=gs://${PROJECT_ID}/ \
 --input-path=data/batch_data \
 --input-files=schools.csv \
 --bq-dataset=liveability
@@ -261,10 +264,10 @@ python3 data_ingestion_configurable.py \
 --max_num_workers=100 \
 --autoscaling_algorithm=THROUGHPUT_BASED \
 --region=us-west1 \
---staging_location=gs://$PROJECT/data/staging \
---temp_location=gs://$PROJECT/data/temp \
---project=$PROJECT \
---input-bucket=gs://$PROJECT/ \
+--staging_location=gs://${PROJECT_ID}/data/staging \
+--temp_location=gs://${PROJECT_ID}/data/temp \
+--project=${PROJECT_ID} \
+--input-bucket=gs://${PROJECT_ID}/ \
 --input-path=data/batch_data \
 --input-files=shoppingcentres.csv \
 --bq-dataset=liveability
@@ -275,10 +278,10 @@ python3 data_ingestion_configurable.py \
 --max_num_workers=100 \
 --autoscaling_algorithm=THROUGHPUT_BASED \
 --region=us-west1 \
---staging_location=gs://$PROJECT/data/staging \
---temp_location=gs://$PROJECT/data/temp \
---project=$PROJECT \
---input-bucket=gs://$PROJECT/ \
+--staging_location=gs://${PROJECT_ID}/data/staging \
+--temp_location=gs://${PROJECT_ID}/data/temp \
+--project=${PROJECT_ID} \
+--input-bucket=gs://${PROJECT_ID}/ \
 --input-path=data/batch_data \
 --input-files=sportsclubs.csv \
 --bq-dataset=liveability
