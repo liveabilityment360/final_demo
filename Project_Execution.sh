8b29fb20b0c2e9bf2bd0af52d922809090326af3
liveabilityment360 <109857290+liveabilityment360@users.noreply.github.com>
2022-09-10 16:51:01 +1000

    Update Project_Execution.sh

--- Project_Execution.sh
+++ Project_Execution.sh
@@ -1,5 +1,5 @@
 #1.create a project and export it into a variable(project creation- manual)
-export PROJECT_ID="ment360-liveability-demo"
+export PROJECT_ID="ment360-liveability-alpha"
       
 #2.Sets the project,location &dataset
 gcloud config set project ${PROJECT_ID}
