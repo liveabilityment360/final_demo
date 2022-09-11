b32ba33790e49caeae0daa7109a7487d77b3f110
liveabilityment360 <109857290+liveabilityment360@users.noreply.github.com>
2022-09-11 18:45:48 +1000

    Update dataflow_run.sh

--- dataflow/dataflow_run.sh
+++ dataflow/dataflow_run.sh
@@ -1,9 +1,9 @@
 #1.create a project and export it into a variable(project creation- manual)
-export PROJECT_ID="ment360-liveability-alpha"
+export PROJECT_ID="mvp-liveability"
       
 #2.Sets the project,location &dataset
 gcloud config set project ${PROJECT_ID}
-export LOCATION="us-west1"
+export LOCATION="australia-southeast1"
 export BQ_DATASET="liveability"
 
 
