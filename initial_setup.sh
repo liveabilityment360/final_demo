2563363c8e65f9dd858669b5fa5f0082e2e7ec2e
liveabilityment360 <109857290+liveabilityment360@users.noreply.github.com>
2022-09-13 20:55:34 +1000

    Update initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,5 +1,5 @@
 #1.create a project and export it into a variable(project creation- manual)
-export PROJECT_ID="ment360live-alpha
+export PROJECT_ID="ment360live-beta
       
 #2.Sets the project,location &dataset
 gcloud config set project ${PROJECT_ID}
