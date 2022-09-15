3f5f5a0777ee8e5b31a0f5c3707d765fec805902
liveabilityment360 <109857290+liveabilityment360@users.noreply.github.com>
2022-09-15 14:17:33 +1000

    Update dataflow_run.sh

--- dataflow/dataflow_run.sh
+++ dataflow/dataflow_run.sh
@@ -1,5 +1,5 @@
 #1.create a project and export it into a variable(project creation- manual)
-export PROJECT_ID="liveability-gama"
+export PROJECT_ID="proj-liveability"
       
 #2.Sets the project,location &dataset
 gcloud config set project ${PROJECT_ID}
